1c97bf9e6ecc9e3b119501fdb6df885f4a25db14
Khyati S <128066844+Khyati-33@users.noreply.github.com>
2024-01-13 16:54:04 +0530

    Update news-app.py

--- news-app.py
+++ news-app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 from newsapi import NewsApiClient
+from bs4 import BeautifulSoup
+import requests
 from datetime import datetime
 from pytz import timezone
-import requests
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 NEWS_API_KEY = 'aee7a2484e074e4b903dd3714f384f2e'
@@ -28,37 +28,57 @@ def index():
 
     # Fetch latest tech news from The Verge
     tech_news = get_verge_tech_news()
+    today_news=get_today_news()
 
     return render_template('index.html', articles=articles, live_date_time=live_date_time, tech_news=tech_news)
 
-def search_articles(query):
-    all_articles = newsapi.get_everything(q=query, language='en', sort_by='relevancy', page_size=5)
-    return all_articles['articles']
-
-
-def get_toi_news():
+def get_today_news():
     toi_url = 'https://timesofindia.indiatimes.com/'
     response = requests.get(toi_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     today_news = []
 
-    # Extract headlines, links, and publication time from Times of India homepage
+    # Extract headlines and links from Times of India homepage
     headlines = soup.find_all('span', class_='w_tle')
     links = soup.find_all('a', class_='w_img')
-    times = soup.find_all('span', class_='update-time')
 
-    for headline, link, time in zip(headlines, links, times):
+    # Get today's date in the format used on the website
+    today_date = datetime.now().strftime('%d %b %Y').lower()
+
+    for headline, link in zip(headlines, links):
         news_title = headline.get_text(strip=True)
         news_link = link['href']
-        news_time = time.get_text(strip=True)
 
-        # Parse the publication time and check if it's today's news
-        news_datetime = datetime.strptime(news_time, '%d %b, %Y, %H:%M %p')
-        today_date = datetime.now().date()
-        if news_datetime.date() == today_date:
-            today_news.append({'title': news_title, 'link': news_link, 'time': news_time})
+        # Extract the date from the link (assuming it's in the format 'yyyy/mm/dd')
+        link_parts = news_link.split('/')
+        news_date = f"{link_parts[3]} {link_parts[4]} {link_parts[5]}".lower()
+
+        # If the news is from today, add it to the list
+        if today_date == news_date:
+            today_news.append({'title': news_title, 'link': news_link, 'time': ''})
 
     return today_news
+
+def search_articles(query):
+    all_articles = newsapi.get_everything(q=query, language='en', sort_by='relevancy', page_size=5)
+    return all_articles['articles']
+
+def get_toi_news():
+    toi_url = 'https://timesofindia.indiatimes.com/'
+    response = requests.get(toi_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    toi_news = []
+
+    # Extract headlines and links from Times of India homepage
+    headlines = soup.find_all('span', class_='w_tle')
+    links = soup.find_all('a', class_='w_img')
+
+    for headline, link in zip(headlines, links):
+        news_title = headline.get_text(strip=True)
+        news_link = link['href']
+        toi_news.append({'title': news_title, 'link': news_link})
+
+    return toi_news
 
 def get_verge_tech_news():
     url = 'https://www.theverge.com/tech'
@@ -71,10 +91,15 @@ def get_verge_tech_news():
     for article in articles:
         news_title = article.find('h2', class_='c-entry-box--compact__title').get_text(strip=True)
         news_link = article.find('a', class_='c-entry-box--compact__link')['href']
-        news_time = article.find('time')['data-chorus-optimize-field']="datehedg" # Extracting the publication time
+        
+        # Extracting the publication time
+        news_time = article.find('time')['data-chorus-optimize-field']
+        
         tech_news.append({'title': news_title, 'link': news_link, 'time': news_time})
 
     return tech_news
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
